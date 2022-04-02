527c199a507724c26de1c11645e9908783aed8b8
Pablo Cavalcante <pablohenriq100@gmail.com>
2022-04-02 19:10:13 -0300

    Company.

---
+++ src/components/company/Company.jsx
@@ -0,0 +1,13 @@
+import Table from "../table/Table";
+
+export default function Company() {
+  return (
+    <div className="container-fluid" style={{ marginTop: "2%" }}>
+      <div className="row d-flex justify-content-center align-items-center">
+        <div className="col-sm-12">
+          <Table />
+        </div>
+      </div>
+    </div>
+  );
+}
